686ea7e97e990c8291ef07fe12ca8866602e4f74
Processos <processos6@fani.com.br>
2026-02-06 09:38:13 -0300

    refactor barcode generation logic for improved readability

--- api/index.py
+++ api/index.py
@@ -14,8 +14,18 @@ def gerar_barcode():
     texto = request.args.get("text")
     if not texto:
         abort(400, "Parâmetro 'text' é obrigatório")
+    writer = ImageWriter()
+    writer_options = {
+        "module_width": 0.33,
+        "module_height": 25.0,
+        "font_size": 10,
+        "text_distance": 3.0,
+        "quiet_zone": 6.5,
+        "dpi": 203,
+        "write_text": True
+    }
 
-    code = barcode.get("code128", texto, writer=ImageWriter())
+    code = barcode.get("code128", texto, writer= writer)
     buffer = io.BytesIO()
     code.write(buffer)
     buffer.seek(0)
